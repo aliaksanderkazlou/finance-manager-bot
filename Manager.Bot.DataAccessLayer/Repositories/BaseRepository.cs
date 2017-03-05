b6bea81ffa9dcfc4de9c1de8c12ac3a304eec92c
Aleksander Kozlov <mrblamber@gmail.com>
2017-03-05 23:36:10 +0300

    Added layers

---
+++ Manager.Bot.DataAccessLayer/Repositories/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Manager.Bot.DataAccessLayer.Repositories
+{
+    public abstract class BaseRepository <T> 
+        where T: class
+    {
+        protected abstract IMongoCollection<T> Items { get; }
+
+        public virtual string GenerateNewId()
+        {
+            return ObjectId.GenerateNewId().ToString();
+        }
+    }
+}
