5e9ec4f8be4251e2e0e612d25aa24986f2e9bd9f
Александр Козлов < >
2017-04-22 18:28:08 +0300

    Fixed mongo auth problem

--- FinanceManager.Bot.DataAccessLayer/MongoService.cs
+++ FinanceManager.Bot.DataAccessLayer/MongoService.cs
@@ -1,21 +1,18 @@
 ﻿using FinanceManager.Bot.DataAccessLayer.Models;
 using FinanceManager.Bot.Helpers.Models;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace FinanceManager.Bot.DataAccessLayer
 {
     public class MongoService
     {
-        private readonly MongoClient _client;
-
         private readonly IMongoDatabase _database;
 
         public MongoService(AppSettings settings)
         {
             var mongoUrl = MongoUrl.Create(settings.DatabaseConnectionString);
-            _client = new MongoClient(settings.DatabaseConnectionString);
-            _database = _client.GetDatabase(mongoUrl.DatabaseName);
+            var client = new MongoClient(settings.DatabaseConnectionString);
+            _database = client.GetDatabase(mongoUrl.DatabaseName);
         }
 
         public IMongoCollection<User> Users => _database.GetCollection<User>("users");
